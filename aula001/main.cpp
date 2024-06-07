40373a2ea7683cca6730859cf2ad27289b4c840d
João Vitor Amaro de Melo <106560064+joaoVitor-amaro@users.noreply.github.com>
2024-06-07 20:06:48 -0300

    Aula 1 de variavel

---
+++ aula001/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+//Variável com um valor constante
+#define pi 3.18
+
+int main(){
+    //Tipo de variáveis
+
+    int vidas = 10; //Varialvel do tipo inteiro. Ex: 10, 12
+    float altura = 1.5; //Varialvel do tipo real. Ex: 1.5, 1.150
+    bool estado = true; ////Varialvel do tipo booleano. Ex: true ou false
+    string nome = "Joao"; //Varialvel do tipo string que aceita qualquer tamanho de caractere
+    char nome1[20] = "V"; //Varialvel do tipo string que suporta 20 caracteres
+
+    cout << pi << endl;
+    return 0;
+}
