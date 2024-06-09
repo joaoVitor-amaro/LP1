40729645bdaafe9ed4ac24d3825c589b30830599
João Vitor Amaro de Melo <106560064+joaoVitor-amaro@users.noreply.github.com>
2024-06-09 16:03:06 -0300

    Aula de estrutura de repetição

---
+++ aula003/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int cont = 0;
+    //While - Estrutura de repeticao
+    while (cont < 5) { //Condição para entrar no bloco
+        cout << cont;
+        cont++;
+    }
+    cout << "" << endl;
+
+    cont = 0;
+    //do while - Estrutura de repeticao
+    //Executa pelo menos 1 vez a instução
+    //no bloco da estrutura
+    do {
+        cout << cont;
+        cont++;
+    } while (cont < 5); //Condição para exercutar a instrução novamente ou não
+    cout << "" << endl;
+    for(int i = 0; i < 5; i++) {
+        cout << i;
+    }
+    return 0;
+}
