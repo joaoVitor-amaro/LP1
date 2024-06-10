35ed0bdfe80ae20852734b2f95017656a8c99123
João Vitor Amaro de Melo <106560064+joaoVitor-amaro@users.noreply.github.com>
2024-06-10 19:23:55 -0300

    Exercicio 11

---
+++ exercicios/ex011/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int cont = 6;
+    int fim = 11;
+    while(cont <= fim) {
+        cout << cont << " ";
+        cont++;
+    }
+    cout << " Acabou";
+    return 0;
+}
