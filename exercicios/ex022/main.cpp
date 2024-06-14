d552ce4cf57b6f335e54d08fd32327a25c13d664
João Vitor Amaro de Melo <106560064+joaoVitor-amaro@users.noreply.github.com>
2024-06-14 10:07:38 -0300

    Exercicio 22

--- exercicios/ex022/main.cpp
+++ exercicios/ex022/main.cpp
@@ -10,6 +10,8 @@ int main(){
     int cont = 0;
     int maiorIdade;
     int qtHomem = 0;
+    int qtMulher = 0;
+    int menorIdadeMulher;
     do {
         cout << "Sexo [F/M]: ";
         cin >> sexo;
@@ -29,13 +31,32 @@ int main(){
         if(sexo == 'M') {
             qtHomem++;
         }
+
+        //
+        if(strcmp(&sexo, "F") == 1) {
+            qtMulher++;
+           //Verifica se esta é a primeira mulher cadastrada
+            if(qtMulher == 1) {
+             //Inicializa 'menorIdadeMulher' com a idade da primeira mulher cadastrada
+                menorIdadeMulher = idade;
+            } else {
+                //Caso contrário, verifica se a idade atual é menor que maiorIdade
+                if(idade < menorIdadeMulher) {
+                    //Se for, atualiza o valor da variavel
+                    menorIdadeMulher = idade;
+                }
+            }
+        }
+
         cout << "Deseja continuar? [S/N]: ";
         cin >> resp;
         //Coloca a string em letra maiuscula
         resp = toupper(resp);
-    } while(strcmp(&resp, "S") == 0);
+        cont++;
+    } while(strcmp(&resp, "S") == 1);
 
     cout << "Maior idade: " << maiorIdade << endl;
     cout << "Quantidade de homens: " << qtHomem << endl;
+    cout << "Menor idade da Mulher: " << menorIdadeMulher << endl;
     return 0;
 }
